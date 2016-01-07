84c19267fc1072dbc9dafc61986f5beaa8b4cb49
Sonic Xiang <Sonic.Xiang@activenetwork.com>
2016-01-07 14:26:08 +0800

    Change masonry directive

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('funMarket', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr', 'masonry']);
+    .module('funMarket', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr', 'wu.masonry']);
 
 })();
